59b7a2f9c5beea9db20d281fd786654fd99f6fda
Ilya <ily4564@yandex.ru>
2025-09-21 12:24:44 +0300

    Правка

--- keyboards/inline_keyboards.py
+++ keyboards/inline_keyboards.py
@@ -12,7 +12,7 @@ button_1_1 = InlineKeyboardButton(text="WB_Orders", callback_data="WB_Orders")
 button_1_2 = InlineKeyboardButton(text="WB_Stock", callback_data="WB_Stock")
 button_2_1 = InlineKeyboardButton(text="Ozon_Orders", callback_data="Ozon_Orders")
 button_2_2 = InlineKeyboardButton(text="Ozon_Stock", callback_data="Ozon_Stock")
-button_back = InlineKeyboardButton(text="Назад", callback_data="Back")
+button_back = InlineKeyboardButton(text="Back", callback_data="Back")
 
 keyboard_WB = InlineKeyboardMarkup(inline_keyboard=[[button_1_1, button_1_2, button_back]])
 keyboard_Ozon = InlineKeyboardMarkup(inline_keyboard=[[button_1_1, button_1_2, button_back]])
