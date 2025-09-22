dad1b1db097ba628be930a1393f03063a1d115c6
Ilya <ily4564@yandex.ru>
2025-09-22 22:36:24 +0300

    Поправил ошибку в инлайн коллбэках

--- keyboards/inline_keyboards.py
+++ keyboards/inline_keyboards.py
@@ -8,10 +8,10 @@ button_2 = InlineKeyboardButton(text="Ozon", callback_data="Ozon")
 # Создаем объект клавиатуры
 keyboard_start = InlineKeyboardMarkup(inline_keyboard=[[button_1, button_2]])
 
-button_1_1 = InlineKeyboardButton(text="WB_Orders", callback_data="WB_Orders")
-button_1_2 = InlineKeyboardButton(text="WB_Stock", callback_data="WB_Stock")
-button_2_1 = InlineKeyboardButton(text="Ozon_Orders", callback_data="Ozon_Orders")
-button_2_2 = InlineKeyboardButton(text="Ozon_Stock", callback_data="Ozon_Stock")
+button_1_1 = InlineKeyboardButton(text="WB_Orders", callback_data="/WB_Orders")
+button_1_2 = InlineKeyboardButton(text="WB_Stock", callback_data="/WB_Stock")
+button_2_1 = InlineKeyboardButton(text="Ozon_Orders", callback_data="/Ozon_Orders")
+button_2_2 = InlineKeyboardButton(text="Ozon_Stock", callback_data="/Ozon_Stock")
 button_back = InlineKeyboardButton(text="Back", callback_data="Back")
 
 keyboard_WB = InlineKeyboardMarkup(inline_keyboard=[[button_1_1, button_1_2, button_back]])
