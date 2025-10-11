e2d4e70cc9743db4d0ca97c1b6ec4e0f11bcfac1
Ilya <ily4564@yandex.ru>
2025-10-11 19:35:33 +0300

    Добавил планировщик задач для автоматического запуска с 5 до 6 утра по мск

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ from config.config import Config, load_config
 from handlers import user
 from keyboards.set_menu import set_menu
 from middlewares.outer import OuterMiddleware
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from pytz import timezone
+from WB_API.ozon_orders_extract import ozon_extract_orders
+from WB_API.wb_stock_history import stock_history_extract
+from WB_API.wb_unite_stock_orders import wb_stock_orders_unite
+from WB_API.merge import union_sales
 
 async def main() -> None:
     # Загружаем конфиг в переменную config
@@ -20,6 +26,18 @@ async def main() -> None:
     # Инициализируем бот и диспетчер
     bot = Bot(token=config.bot.token)
     dp = Dispatcher()
+
+    # создаём планировщик для asyncio
+    scheduler = AsyncIOScheduler(timezone=timezone("Europe/Moscow"))
+
+    # добавляем задачу: каждый день в 05:00 по Москве
+    scheduler.add_job(ozon_extract_orders, 'cron', hour=5, minute=0)
+    scheduler.add_job(union_sales, 'cron', hour=5, minute=10)
+    scheduler.add_job(stock_history_extract, 'cron', hour=5, minute=20)
+    scheduler.add_job(wb_stock_orders_unite, 'cron', hour=5, minute=35)
+
+    # запускаем планировщик
+    scheduler.start()
 
     # Настраиваем кнопку меню
     await set_menu(bot)
